a1c84f363df70a9b2cce20af6e444fa37ac6c3cd
yale8848 <royal8848@163.com>
2017-12-11 18:21:16 +0800

    add service

--- app/src/main/java/ren/yale/android/easyincrementalupdate/MainActivity.java
+++ app/src/main/java/ren/yale/android/easyincrementalupdate/MainActivity.java
@@ -2,17 +2,10 @@ package ren.yale.android.easyincrementalupdate;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.daoxuehao.android.commondir.CommonDir;
-
-import java.io.File;
-
-import ren.yale.android.intremetalupdate.EasyIncrementalUpdate;
 
 public class MainActivity extends Activity {
 
@@ -27,34 +20,9 @@ public class MainActivity extends Activity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
 
-        File file = CommonDir.getInstance().getExternalStorageDirectoryWrite("111");
 
-        final String path = file.getAbsolutePath()+File.separator;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        startService(new Intent(this,TestService.class));
 
-                try {
-                    ApplicationInfo appInfo = MainActivity.this.getPackageManager()
-                            .getApplicationInfo(MainActivity.this.getPackageName(), 0);
-
-                   if ( EasyIncrementalUpdate.patch(appInfo.sourceDir,path+"aaa.apk",path+"p.apk")){
-                       Intent intent = new Intent(Intent.ACTION_VIEW);
-                       intent.setDataAndType(Uri.parse("file://" + path+"aaa.apk"),
-                               "application/vnd.android.package-archive");
-                       MainActivity.this.startActivity(intent);
-                   }else{
-                       Log.d(TAG," update fail");
-                   }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-
-
-            }
-        }).start();
     }
 
 }
--- library/src/main/cpp/err.h
+++ library/src/main/cpp/err.h
@@ -7,28 +7,53 @@
 
 #include "log.h"
 
-
+/*
 #define err(n,format,...) {\
-        LOGE(format,__VA_ARGS__);       \
+        if (format == NULL) return n;\
+        LOGE_(format,__VA_ARGS__);       \
         return n;                      \
 }                                       \
+
+#define err(n,format) {\
+        if (format == NULL) return n;\
+        LOGE_(format,__VA_ARGS__);       \
+        return n;                      \
+}                                       \
+
+#define errx(n,format,...) {\
+        if (format == NULL) return n;\
+        LOGE_(format,__VA_ARGS__);       \
+        return n;                      \
+}                                      \
+
+
+#define errx(n,format) {\
+        if (format == NULL) return n;\
+        LOGE_(format,__VA_ARGS__);       \
+        return n;                      \
+}                                      \
+
+*/
+
 //#include<stdarg.h>
 //#include <stdio.h>
 
-int err1(int n, char * format,...)
-{
-    LOGE(format);
-    return  n;
+
+int err(int n,char *format,char *value){
+        if (format == NULL){
+                return n;
+        }
+        LOGE_(format, value);
+        return  n;
 }
 
-int errx(int n, char * format,...)
-{
-    LOGE(format);
-    return  n;
+int errx(int n,char *format,char *value){
+        if (format == NULL){
+                return n;
+        }
+        LOGE_(format, value);
+        return  n;
 }
-
-
-
 //#define err(n, ...) _exit(n)
 //#define errx(n, ...) _exit(n)
 
