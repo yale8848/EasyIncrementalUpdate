dda91b9b2f8afb729c8db673cc733dcc7f928f44
yale8848 <royal8848@163.com>
2017-12-12 11:38:15 +0800

    add text version

--- app/src/main/java/ren/yale/android/easyincrementalupdate/MainActivity.java
+++ app/src/main/java/ren/yale/android/easyincrementalupdate/MainActivity.java
@@ -1,15 +1,18 @@
 package ren.yale.android.easyincrementalupdate;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.daoxuehao.android.commondir.CommonDir;
 
 public class MainActivity extends Activity {
 
-    static final String TAG="easyincrementalupdate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,26 @@ public class MainActivity extends Activity {
         CommonDir.getInstance().init(this);
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-
+        tv.setText(getAppVersionName(this));
 
         startService(new Intent(this,TestService.class));
 
+    }
+
+    public static String getAppVersionName(Context context) {
+        String versionName = "";
+        try {
+            // ---get the package info---
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            versionName = pi.versionName;
+            if (versionName == null || versionName.length() <= 0) {
+                return "";
+            }
+        } catch (Exception e) {
+            Log.e("VersionInfo", "Exception", e);
+        }
+        return versionName;
     }
 
 }
