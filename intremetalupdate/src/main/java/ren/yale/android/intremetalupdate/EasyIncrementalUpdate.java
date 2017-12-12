7bef64e23235ac7336e0d1d26a89a01da6ebff25
yale8848 <royal8848@163.com>
2017-12-12 16:50:07 +0800

    change lib name

--- intremetalupdate/src/main/java/ren/yale/android/intremetalupdate/EasyIncrementalUpdate.java
+++ intremetalupdate/src/main/java/ren/yale/android/intremetalupdate/EasyIncrementalUpdate.java
@@ -12,12 +12,25 @@ public class EasyIncrementalUpdate {
     static {
         System.loadLibrary("eiu-bspatch");
     }
+
+    /**
+     * @param oldApkPath old apk path
+     * @param newApkPath  new created apk path
+     * @param patchApkPath patch apk path
+     * @return
+     */
     public synchronized static native boolean patch(String oldApkPath, String newApkPath, String patchApkPath);
 
+    /**
+     * @param context
+     * @param newApkPath
+     * @param patchApkPath
+     * @return
+     */
     public synchronized static  boolean patch(Context context,String newApkPath, String patchApkPath){
         try {
-            ApplicationInfo appInfo = context.getPackageManager()
-                    .getApplicationInfo(context.getPackageName(), 0);
+            ApplicationInfo appInfo = context.getApplicationContext().getPackageManager()
+                    .getApplicationInfo(context.getApplicationContext().getPackageName(), 0);
             return patch(appInfo.sourceDir,newApkPath,patchApkPath);
         } catch (Exception e) {
             e.printStackTrace();
